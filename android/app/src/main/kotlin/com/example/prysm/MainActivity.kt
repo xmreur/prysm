c9aa45a46127bb0c85cd75dc0de411b4c68bc965
xmreur <xmreur@gmail.com>
2025-11-09 01:38:27 +0100

    v0.0.7 - Android Integration

--- android/app/src/main/kotlin/com/example/prysm/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package github.xrmeur.prism
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/xmreur/prysm/TorController.kt
@@ -0,0 +1,4 @@
+package com.xmreur.prysm
+
+class TorController {
+}

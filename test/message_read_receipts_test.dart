cd879a99b4bcb90346ecb1da34fef1056ab1d477
xmreur <xmreur@gmail.com>
2026-06-09 00:43:14 +0200

    fix: removed unused import from test file

--- test/message_read_receipts_test.dart
+++ test/message_read_receipts_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:prysm/database/message_read_receipts.dart';
 import 'package:prysm/database/messages.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 void main() {
